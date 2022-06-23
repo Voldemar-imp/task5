4796fa3e2d6c6f3d73400f2068a9bac6788c7a3a
Voldemar-imp <108013210+Voldemar-imp@users.noreply.github.com>
2022-06-23 17:15:17 +0300

    Add files via upload

---
+++ Program5.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Kutuzov";
+            string secondName = "Vladimir";
+            Console.WriteLine("До : " + "name = " + name + " secondName = " + secondName);
+           
+            string swapp;
+            swapp = name;
+            name = secondName;
+            secondName = swapp;
+
+            Console.WriteLine("После : " + "name = " + name + " secondName = " + secondName);            
+        }
+    }
+}
